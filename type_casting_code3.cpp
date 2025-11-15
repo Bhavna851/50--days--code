1594be45649f974f81bdf9cbf1bd5f5424c8e470
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-15 19:53:59 +0530

    Create type_casting_code3.cpp
    adding code3

---
+++ type_casting_code3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //implicit conversion(conversion of smaller datatype to bigger datatype)
+    char grade='A';
+    int value1=grade;
+    cout<<"value is: "<<value1<<endl;//Ascii value of A
+    //explicit conversion(conversion of bigger datatype to smaller datatype)
+     double d=3.14;
+     int value2=(int)d;//conversion by assignment operator
+     cout<<"value is: "<<value2<<endl;
+     return 0;
+
+}
