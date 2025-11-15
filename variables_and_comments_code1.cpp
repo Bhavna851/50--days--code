ab12ec89b2889bd9c025d85aa861171ed12db7d4
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-15 18:58:22 +0530

    Create variables_and_comments_code1.cpp
    adding code1

---
+++ variables_and_comments_code1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int v=2;//variable
+    cout<<"value of variable is: "<<v<<endl;
+    v=4;
+    cout<<"now value of variable is: "<<v<<endl;
+    const int c=7;//constant
+    cout<<"value of constant is: "<<c<<endl;
+    //c=5; thse line will give an error because the value of constant can never be changed 
+    return 0;
+
+}
