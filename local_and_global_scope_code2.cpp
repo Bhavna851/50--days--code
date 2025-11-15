48b91799372db99e4eb22824fbd222d17d4fdee0
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-15 19:43:49 +0530

    Create local_and_global_scope_code2.cpp
    adding code2

---
+++ local_and_global_scope_code2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int global=34;
+int main(){
+    int local=22;
+    cout<<"local variable is: "<<local<<endl;
+    cout<<"global variable is: "<<global<<endl;
+    return 0;
+    
+}
