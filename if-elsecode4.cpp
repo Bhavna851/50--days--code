2e8c471c41763e917569a220f6fe6e24b6b4464f
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-15 21:04:50 +0530

    Create if-elsecode4.cpp
    adding code4

---
+++ if-elsecode4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int marks;
+  cout<<"enter marks of student: "<<endl;
+  cin>>marks;
+  if(marks>=90){
+   cout<<"A"<<endl;
+
+  }
+   else if(70<=marks&&marks<90){
+      cout<<"B"<<endl;
+   }
+   else
+   {
+      if(50<=marks&&marks<70){
+         cout<<"C"<<endl;
+      }
+      else{
+         cout<<"D"<<endl;
+      }
+   }
+   return 0;
+}
+   
