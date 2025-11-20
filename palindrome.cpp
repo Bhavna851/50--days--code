ecbcf9a2c8f759985168e7132447141bb781cde9
Bhavna shriwas <bhavnashriwas851@gmail.com>
2025-11-20 22:18:34 +0530

    todays code

---
+++ palindrome.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int checkpalindrome(string s){
+    string s1;
+    int size=s.size();
+    cout<<size<<endl;
+for(int i=size-1;i>=0;i--){
+     s1.push_back(s[i]);
+     
+}cout<<s1<<endl;
+  if(s==s1){
+        cout<<"palindrome"<<endl;
+     }
+     else{
+        cout<<"Not plaindrome"<<endl;
+     }
+}
+int main(){
+    string s;
+    getline(cin,s);
+    checkpalindrome(s);
+    return 0;
+}
---
+++ reverseofnumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//reverse of a number
+int reverseofnum(int num){
+    int ld;
+    int reverse;
+    while(num>0){
+         ld=num%10;
+        num=num/10;
+         cout<<ld;
+    }
+}
+int main(){
+    int num;cin>>num;
+    reverseofnum(num);
+    
+    return 0;
+}
